0589d57ce328d66ff88df9f9fd37c4df880368fb
Georgiana21 <ichim.georgianav@gmail.com>
2016-06-03 20:20:28 +0300

    mock
    testare cu obiecte mock

--- src/ichim/proiect/cts/junit/TestComandaSuma.java
+++ src/ichim/proiect/cts/junit/TestComandaSuma.java
@@ -1,9 +1,12 @@
 package ichim.proiect.cts.junit;
 
+import static org.mockito.Mockito.*;
 import javax.activation.MimeTypeParameterList;
 
+import ichim.proiect.cts.Carte;
 import ichim.proiect.cts.Comanda;
 import ichim.proiect.cts.Film;
+import ichim.proiect.cts.Muzica;
 import ichim.proiect.cts.Produs;
 import junit.framework.TestCase;
 
@@ -62,6 +65,20 @@ public class TestComandaSuma extends TestCase {
 		long afterCall=System.currentTimeMillis();
 		if(afterCall-beforeCall>0.05)
 			fail("Timp prea mare de executie!");
+	}
+	
+	public void testMock(){
+		Produs film=mock(Film.class);
+		when(film.pretFinal()).thenReturn(10.0);
+		Produs carte=mock(Carte.class);
+		when(carte.pretFinal()).thenReturn(20.0);
+		Produs muzica=mock(Muzica.class);
+		when(muzica.pretFinal()).thenReturn(30.0);
+		comanda.adaugaProdus(film);
+		comanda.adaugaProdus(carte);
+		comanda.adaugaProdus(muzica);
+		double valoareAsteptata=60.0;
+		assertEquals("Verificare metoda cu mock", valoareAsteptata,comanda.calculSumaDePlata());
 	}
 
 }
